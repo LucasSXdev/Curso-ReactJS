f57fc72be8debd62b62733f44399292f8f81b324
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-08-15 13:09:18 -0300

    adicionando a aula2

---
+++ 02-JSX/index.jsx
@@ -0,0 +1,18 @@
+/*
+-JSX é uma extensão do javascript
+-É usado pelo react para representar os elementos do DOM.
+-Facilita o processo de escrita dos componentes que serão renderizados na tela.
+*/
+
+const h1 = <h1>Olá,mundo!</h1>;
+
+const anotherElement = (
+  <div>
+    <h1>Texto</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+);
