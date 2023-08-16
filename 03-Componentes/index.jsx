722b6f3ad4840dfc2e10acc87963bea8dd59deb4
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-08-16 04:03:35 -0300

    adicionando a aula 3

---
+++ 03-Componentes/index.jsx
@@ -0,0 +1,19 @@
+function PrimaryButton() {
+  return <button>Meu primeiro botão</button>;
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <h1>conteúdo principal</h1>
+    </div>
+  );
+}
+
+/*
+A sintaxe para criação de um componente no react é como o código acima
+-Sao criados através de funções
+-A convenção é sempre começar com letra maiúscula
+-A função retorna um componente
+-Para nomear classes e id´s,é um pouco diferente, utilizando a sintaxe do js em vez do html(ex:className,Htmlfor..etc)
+*/
