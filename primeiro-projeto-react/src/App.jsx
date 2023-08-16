dc9201e01d255fe3ebb1a0f8ba7b5b184c08bfdd
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-08-16 05:08:14 -0300

    adicionando a aula 5

---
+++ primeiro-projeto-react/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Olá,mundo!</h1>
+      <p>Este é o meu primeiro projeto utilizando react e vit</p>
+      <p>próximos passos:</p>
+      <ul>
+        <li>praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>estilizar os componentes</li>
+        <li>Reutilizar os componentes</li>
+      </ul>
+    </div>
+  );
+}
