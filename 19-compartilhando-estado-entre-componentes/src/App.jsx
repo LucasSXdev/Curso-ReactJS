4f6f9d70902770e4d0d4a9e867df368307f22324
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-08-30 12:38:39 -0300

    adicionando a aula19

---
+++ 19-compartilhando-estado-entre-componentes/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Input from "./components/Input";
+
+export default function App() {
+  const [password, setPassword] = useState("");
+  const [copyText, setCopyText] = useState("Copiar");
+  const [passwordSize, setPasswordSize] = useState(12);
+
+  function generatePwd() {
+    let characteres =
+      "qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM123456789";
+    const length = passwordSize;
+    let newPwd = "";
+    for (let i = 0; i < length; i++) {
+      const position = Math.floor(Math.random() * characteres.length);
+      newPwd += characteres[position];
+    }
+    setPassword(newPwd);
+    setCopyText("Copiar");
+  }
+
+  function copyToClipboard() {
+    window.navigator.clipboard.writeText(password);
+    setCopyText("Copiado");
+  }
+  return (
+    <div className="app">
+      <h1>Gerador de senhas</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho</label>
+        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+      </div>
+      <button onClick={generatePwd}>
+        Gerar senha de {passwordSize} caracteres
+      </button>
+      <button onClick={copyToClipboard}>{copyText}</button>
+
+      <div>{password}</div>
+    </div>
+  );
+}
+
+//Para utilizarmos o state de um componente externo, que esteja em outra pasta devemos:
+//Utilizar na função que retorna o componente o props."nome da variavel ou função"
+//Depois devemos manter o state desse componente no arquivo principal
+//Por fim devemos referenciar as variaveis no arquivo principal
