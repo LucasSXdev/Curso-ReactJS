76f1291550f720de4f89a1f9bdb92c6b7c4ee62a
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-09-03 14:25:54 -0300

    alterando a aula 20

--- 20-renderizacao-condicional/src/App.jsx
+++ 20-renderizacao-condicional/src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   const [password, setPassword] = useState("");
   const [copyText, setCopyText] = useState("Copiar");
   const [passwordSize, setPasswordSize] = useState(12);
+  const [showInput, setShowInput] = useState(false);
 
   function generatePwd() {
     let characteres =
@@ -27,9 +28,23 @@ export default function App() {
     <div className="app">
       <h1>Gerador de senhas</h1>
       <div>
-        <label htmlFor="passwordSize">Tamanho</label>
-        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+        <label htmlFor="showInput">Customizar Tamanho</label>
+        <input
+          type="checkbox"
+          id="showInput"
+          onChange={() => setShowInput((currentState) => !currentState)}
+        />
       </div>
+      {showInput ? (
+        <div>
+          <label htmlFor="passwordSize">Tamanho</label>
+          <Input
+            passwordSize={passwordSize}
+            setPasswordSize={setPasswordSize}
+          />
+        </div>
+      ) : null}
+
       <button onClick={generatePwd}>
         Gerar senha de {passwordSize} caracteres
       </button>
