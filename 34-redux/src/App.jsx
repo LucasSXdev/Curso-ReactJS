30a71f34887473a88eaf753c0c8229e17ed226e3
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-11-14 22:27:18 -0300

    adicionando a aula de redux e configurando o projeto

---
+++ 34-redux/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div>
+      meu nome é ... e tenho ... anos
+      <br />
+      tema:...
+      <hr />
+      <input type="text" value={"..."} />
+      <hr />
+      <button>switch theme</button>
+    </div>
+  );
+}
