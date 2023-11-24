b65a2390d5df8b6bd15c72d176c5c5b3cece09f0
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-11-23 23:41:12 -0300

    adicionando uma action

--- 34-redux/src/redux/rootReducer.js
+++ 34-redux/src/redux/rootReducer.js
@@ -3,4 +3,8 @@ import userReducer from "./user/slice";
 
 export default combineReducers({
   user: userReducer,
+  createUser: (state, action) => {
+    console.log(...state);
+    return { ...state };
+  },
 });
